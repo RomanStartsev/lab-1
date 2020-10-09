df5ffb12d9887a025d17b486ca90f66702c03a39
RomanStartsev <70750384+RomanStartsev@users.noreply.github.com>
2020-10-09 12:38:49 +0300

    Add files via upload

---
+++ 2(11).cpp
@@ -0,0 +1,27 @@
+﻿// ConsoleApplication5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+	// Zadanie 2.11
+	int  a, b, c, d;
+	a = 5;
+	b = 3;
+	c = 2;
+	d = 4;
+	!(a > b);
+	printf("a=%d; b=%d\n", a, b);
+	!(2 * a == b + 4);
+	printf("a=%d; b=%d\n", a, b);
+	!(a < b&& c < d);
+	printf("a=%d; b=%d\n", a, b);
+	!(a < 2 || a>5);
+	printf("a=%d; b=%d\n", a, b);
+	!(a < 1 || b < 2 && c < 3);
+	printf("a=%d; b=%d\n", a, b);
+}
+		 		
+
+
