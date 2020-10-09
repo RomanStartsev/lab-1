56d665d3b03f95bfdf2d244241d19d539201188d
RomanStartsev <70750384+RomanStartsev@users.noreply.github.com>
2020-10-09 10:55:25 +0300

    Add files via upload

---
+++ 2(6).cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+
+int main()
+{
+	// Zadanie 2.6
+	int E1, E2;
+	E1= 1;
+	E2 = 2;
+	E1 = E1 + E2;
+	printf("E1=%d; E2=%d\n", E1, E2);
+	E1 = E1 + (E2);
+	printf("E1=%d; E2=%d\n", E1, E2);
+}
